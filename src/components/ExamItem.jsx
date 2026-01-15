6541bf280e7df19b06aa64c9fba34cb0b9da51a3
Amina151004 <henniamina10@gmail.com>
2026-01-15 01:03:00 +0100

    fix db problem + profile res + dash ress

--- src/components/ExamItem.jsx
+++ src/components/ExamItem.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const ExamItem = ({ name }) => (
   <div className="flex justify-between items-center py-6 border-b border-gray-200">
     <span className="text-gray-900 font-medium">{name}</span>
-    <div className="flex gap-3">
+    <div className="flex gap-1">
       <button className="px-6 py-2 rounded-full bg-[#1a2742] text-white font-medium text-sm">Sujet</button>
       <button className="px-6 py-2 rounded-full bg-[#1a2742] text-white font-medium text-sm">Correction</button>
+      <button className="px-6 py-2 rounded-full bg-[#1a2742] text-white font-medium text-sm">liste etudiant</button>
     </div>
   </div>
 );
